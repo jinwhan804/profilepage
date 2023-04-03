c406a151a484376302d0bbce222650430634ee85
jinwhan804 <jinwhan804@hanmail.net>
2023-04-03 17:40:51 +0900

    내용 수정 및 헤드라인 효과 추가

--- index.js
+++ index.js
@@ -9,9 +9,29 @@ function menu(){
 
     // 메뉴 버튼 하이라이트 생성
     let menuBtn = document.querySelector(".menu-btn");
-    if(menuBtn.classList.contains(".highlight")){
-        menuBtn.classList.remove(".highlight");
+    if(menuBtn.classList.contains("highlight")){
+        menuBtn.classList.remove("highlight");
     }else{
-        menuBtn.classList.add(".highlight");
+        menuBtn.classList.add("highlight");
     }
+}
+
+let _moveBtns = document.querySelectorAll(".moveBtn");
+
+function moveAfter(){
+    let headerExpend = document.querySelector(".header");   
+    
+    let menuBtn = document.querySelector(".menu-btn");
+     
+    _moveBtns.forEach(function(i,a){
+        _moveBtns[a].addEventListener("click",function(){
+            if(headerExpend.classList.contains("isExpend")){
+                headerExpend.classList.remove("isExpend");
+            }
+
+            if(menuBtn.classList.contains("highlight")){
+                menuBtn.classList.remove("highlight");
+            }
+        })
+    })
 }
